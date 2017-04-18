f912f8d203be99a10986479fe1ad2046a73d98ed
PawelKawecki <pawel.kawecki@miquido.com>
2017-04-18 13:11:50 +0200

    Seeders, dummy data

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(\App\Thread::class, function ($faker) {
+    return [
+        'user_id' => function () {
+            return factory(\App\User::class)->create()->id;
+        },
+        'title'   => $faker->sentence,
+        'body'    => $faker->paragraph,
+    ];
+});
+
+$factory->define(\App\Reply::class, function ($faker) {
+    return [
+        'thread_id' => function () {
+            return factory(\App\Thread::class)->create()->id;
+        },
+        'user_id'   => function () {
+            return factory(\App\User::class)->create()->id;
+        },
+        'body'      => $faker->paragraph,
     ];
 });
